9a865c668896a16966c89dc7aeedf1675a7cc20c
YCH <yrkn88@gmail.com>
2023-12-02 20:38:29 +0300

    Init Commit

--- App.js
+++ App.js
@@ -1,33 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import QRCode from 'react-native-qrcode-svg'
+import React, { useState } from 'react';
+import Menu from './Menu';
+import BackgroundImg from './BackgroundImage'; 
+import Categories from './Categories';
+import items from './data';
+import './index.css';
+const allCategories = ['all',...new Set(items.map(item => item.category))];
 
-export default function App() {
-  getAllMenu();  
-  return (
-    <View style={styles.container}>
-      <Text>Hello Traveler!</Text>
-      <StatusBar style="auto" />
-      <QRCode
-    value= "https://imkeskin.wixsite.com/bubble-tea"
-  />
-    </View>
-    
-  );
+function App() {
+  const [menuItems,setMenuItems] = useState(items)
+  const [categories, setCategories] = useState(allCategories)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(items)
+      return
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+
+  return <main >
+    <section className='menu section'>
+      <div className='title'>
+        <h2>our menu</h2>
+        <div className='underline'></div>
+      </div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menuItems} />
+    </section>
+  </main>;
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-  function getAllMenu() {
-    fetch(`https://reactcafetestapi.000webhostapp.com/`, {
-    }).then(res => res.json()).then((res)=>console.log(res))
-    .catch((error) => {console.log(error)});
-  };
- 
+export default App;
---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BackgroundImg>
+    <App />
+   </BackgroundImg>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
